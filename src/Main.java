59ed315886ad777562ed77db729b9f997e77442e
Denis Shipiguzev <d.shipiguzev@yandex.ru>
2024-01-09 17:31:34 +0700

    edit gitignore

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Поехали!");
         /*
 1. Создайте две задачи, а также эпик с двумя подзадачами и эпик с одной подзадачей.
 */
